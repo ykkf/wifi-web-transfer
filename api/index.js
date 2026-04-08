0789262ac40fe2409775e28a661b136387b23996
YK <yusukeft@yahoo.co.jp>
2026-04-08 09:56:08 +0900

    変更

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const uploadRoute = require('./upload');
 const filesRoute = require('./files');
@@ -8,12 +9,25 @@ const downloadRoute = require('./download');
 const app = express();
 app.use(cors());
 
-// Accept large JSON bodies (base64 files can be big)
+// Accept large JSON bodies (base64 files)
 app.use(express.json({ limit: '100mb' }));
 
-// Main routing
+// Serve static files from public/
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// API routing
 app.use('/api/upload', uploadRoute);
 app.use('/api/files', filesRoute);
 app.use('/api/download', downloadRoute);
+
+// Files download page
+app.get('/files/:id', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'files.html'));
+});
+
+// Fallback to index
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
